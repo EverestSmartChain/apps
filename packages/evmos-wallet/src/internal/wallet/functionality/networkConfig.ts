82eb8e3d9ca57d459a922c5eb0ceefcce43b464e
EverestSmartChain <147171570+EverestSmartChain@users.noreply.github.com>
2023-10-23 16:56:57 +0545

    Update networkConfig.ts

--- packages/evmos-wallet/src/internal/wallet/functionality/networkConfig.ts
+++ packages/evmos-wallet/src/internal/wallet/functionality/networkConfig.ts
@@ -2,33 +2,33 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 export const EVMOS_RPC_URL =
-  process.env.NEXT_PUBLIC_EVMOS_RPC_URL || "https://evmos.lava.build/";
+  process.env.NEXT_PUBLIC_EVMOS_RPC_URL || "http://158.69.35.30:26657";
 export const EVMOS_GRPC_URL =
-  process.env.NEXT_PUBLIC_EVMOS_GRPC_URL || "https://rest.evmos.lava.build/";
+  process.env.NEXT_PUBLIC_EVMOS_GRPC_URL || "http://158.69.35.30:26657";
 export const EVMOS_CHAIN_NAME =
-  process.env.NEXT_PUBLIC_EVMOS_CHAIN_NAME ?? "Evmos";
-export const EVMOS_SYMBOL = process.env.NEXT_PUBLIC_EVMOS_SYMBOL ?? "EVMOS";
+  process.env.NEXT_PUBLIC_EVMOS_CHAIN_NAME ?? "Everest Smart Chain";
+export const EVMOS_SYMBOL = process.env.NEXT_PUBLIC_EVMOS_SYMBOL ?? "EVT";
 export const EVMOS_DECIMALS = parseInt(
   process.env.NEXT_PUBLIC_EVMOS_DECIMALS ?? "18"
 );
 export const EVMOS_COSMOS_EXPLORER =
-  process.env.NEXT_PUBLIC_EVMOS_COSMOS_EXPLORER ?? "https://escan.live/";
+  process.env.NEXT_PUBLIC_EVMOS_COSMOS_EXPLORER ?? "https://evtscan.com/";
 export const EVMOS_ETH_CHAIN_ID =
   process.env.NEXT_PUBLIC_EVMOS_ETH_CHAIN_ID ?? "0x2329";
 export const EVMOS_CHAIN = {
-  chainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID ?? "9001"),
-  cosmosChainId: process.env.NEXT_PUBLIC_COSMOS_CHAIN_ID ?? "evmos_9001-2",
+  chainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID ?? "8848"),
+  cosmosChainId: process.env.NEXT_PUBLIC_COSMOS_CHAIN_ID ?? "evt_8848-1",
 };
 
 export const EVMOS_NETWORK_FOR_BACKEND =
-  process.env.NEXT_PUBLIC_EVMOS_NETWORK_FOR_BACKEND ?? "EVMOS";
+  process.env.NEXT_PUBLIC_EVMOS_NETWORK_FOR_BACKEND ?? "Everest Smart Chain";
 export const EVMOS_BACKEND =
   process.env.NEXT_PUBLIC_EVMOS_BACKEND ?? "https://goapi-staging.evmos.org";
 
 export const OSMOSIS_CHAIN_ID =
   process.env.NEXT_PUBLIC_OSMOSIS_CHAIN_ID ?? "osmosis-1";
 export const EVMOS_MINIMAL_COIN_DENOM =
-  process.env.NEXT_PUBLIC_EVMOS_MINIMAL_COIN_DENOM ?? "aevmos";
+  process.env.NEXT_PUBLIC_EVMOS_MINIMAL_COIN_DENOM ?? "aevt";
 
 export const WALLET_CONNECT_PROJECT_ID =
   process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ??
